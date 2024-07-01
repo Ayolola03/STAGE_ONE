bb4e247066c589096854eee837a17aa8487b8c61
Tomilola <83653154+Ayolola03@users.noreply.github.com>
2024-07-01 12:23:16 +0100

    temperature

--- server/views.py
+++ server/views.py
@@ -23,12 +23,26 @@ def hello(request):
     geo_url = f"https://geo.ipify.org/api/v1?apiKey={api_key}&ipAddress={client_ip}"
 
     try:
+        # For the location and region
         response = requests.get(geo_url)
-        response.raise_for_status()  # Raise an error for bad status codes
+        response.raise_for_status()  
         data = response.json()
         location = f"{data.get('location', {}).get('region', 'Unknown')}, {data.get('location', {}).get('country', 'Unknown')}"
+
+        #For the locations longitude and latitude
+        weather_api_key = "1c56e2d2a1dcda840a5183eec122b8b2"
+        lat = data.get("location", {}).get("lat")
+        lon = data.get("location", {}).get("lng")
+        weather_url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={weather_api_key}&units=metric"
+
+        #For the temperature based on the longitude and latitude
+        response_weather = requests.get(weather_url)
+        response_weather.raise_for_status()
+        data_weather = response_weather.json()
+        temperature = data_weather.get("main", {}).get("temp", "Unknown")
     except requests.exceptions.RequestException as e:
         location = "Unknown"
+        temperature = "Unknown"
 
     temperature = "11 Degrees Celsius"
     greeting = f"Hello {visitor_name}!, the temperature is {temperature} in {location}."
