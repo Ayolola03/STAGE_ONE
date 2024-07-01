287fa953f5dde3ffd25aac044071a42a97ba328f
Tomilola <83653154+Ayolola03@users.noreply.github.com>
2024-07-01 10:32:09 +0100

    iplocation

--- server/views.py
+++ server/views.py
@@ -3,32 +3,6 @@ import requests
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 
-# Create your views here.
-
-
-@api_view(["GET"])
-def hello(request):
-    visitor_name = request.GET.get("visitor_name", "Guest")
-    client_ip = request.META.get("REMOTE_ADDR")
-
-    response = requests.get(
-        f"https://api.ipdata.co/{client_ip}?api-key="
-    )
-    data = response.json()
-    location = data.get("city", "Unknown")
-    temperature = "11 Degress Celsuis"
-    greeting = f"Hello {visitor_name}, your location is {location} and the temperature is {temperature}"
-
-    return JsonResponse(
-        {"client_ip": client_ip, "location": location, "greeting": greeting}
-    )
-
-from django.shortcuts import render
-import requests
-from django.http import JsonResponse
-from rest_framework.decorators import api_view
-import os
-
 
 def get_client_ip(request):
     x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
