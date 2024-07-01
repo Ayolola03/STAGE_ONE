11fc8c728c133ec92d12d0cb0b213b9643813abc
Tomilola <83653154+Ayolola03@users.noreply.github.com>
2024-07-01 10:13:02 +0100

    iplocation

--- server/views.py
+++ server/views.py
@@ -11,7 +11,9 @@ def hello(request):
     visitor_name = request.GET.get("visitor_name", "Guest")
     client_ip = request.META.get("REMOTE_ADDR")
 
-    response = requests.get(f"http://ip-api.com/json/{client_ip}")
+    response = requests.get(
+        f"https://api.ipdata.co/{client_ip}?api-key=c93e43f837557bf83551058e095c0fe5f32dc54adaf84895fcf2a821"
+    )
     data = response.json()
     location = data.get("city", "Unknown")
     temperature = "11 Degress Celsuis"
